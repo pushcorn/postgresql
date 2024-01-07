e6084e3a276ab241a6d1a98223b032b487ee12f2
Joseph Tzeng <joseph@pushcorn.com>
2024-01-07 13:29:26 -0500

    adds the db provider to postgresql.Api.Context

--- test/tests/lib/postgresql/Api.js
+++ test/tests/lib/postgresql/Api.js
@@ -1,16 +1,13 @@
-const Context = nit.require ("http.Context");
-
-
 test.method ("postgresql.Api", "dispatch")
     .should ("register the database provider")
         .up (s => s.class = s.class.defineSubclass ("MyApi")
             .onDispatch (ctx =>
             {
-                s.db = ctx.lookupService ("postgresql.Database");
+                s.db = ctx.db;
             })
         )
         .mock ("postgresql.Database.prototype", "disconnect")
-        .given (Context.new ())
+        .before (s => s.args = s.http.Context.new ())
         .after (s => s.args[0].destroy ())
         .expectingPropertyToBeOfType ("db", "postgresql.Database")
         .expectingPropertyToBe ("mocks.0.invocations.length", 1)
