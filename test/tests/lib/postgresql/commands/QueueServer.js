82f4daec927310f99cf24469ba96f29709e70945
Joseph Tzeng <joseph@pushcorn.com>
2024-01-08 11:57:23 -0500

    adds the QueueServer command

---
+++ test/tests/lib/postgresql/commands/QueueServer.js
@@ -0,0 +1,8 @@
+test.command ("postgresql:queue-server")
+    .should ("start the queue server")
+        .up (s => s.Logger = nit.require ("plugins.Logger"))
+        .mock ("Logger.Logger.prototype", "writeLog")
+        .after (s => s.object.server.stop ())
+        .expectingPropertyToBe ("mocks.0.invocations.length", 3)
+        .commit ()
+;
